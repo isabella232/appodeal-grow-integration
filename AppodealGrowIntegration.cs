ab535661e8e183535c1d76b4a294c78a644c388a
Nazar Kozak <nzrkzk@gmail.com>
2016-04-04 05:37:47 +0300

    class names update in bindEvents and events update

--- AppodealGrowIntegration.cs
+++ AppodealGrowIntegration.cs
@@ -1,12 +1,3 @@
-/*
- * Copyright (C) 2012-2016 Soomla Inc. - All Rights Reserved
- *
- *   Unauthorized copying of this file, via any medium is strictly prohibited
- *   Proprietary and confidential
- *
- *   Written by Refael Dakar <refael@soom.la>
- */
-
 using Grow.Integrations.Extensions;
 using UnityEngine;
 
@@ -53,21 +44,91 @@ namespace Grow.Integrations
 		}
 
 		public override void bindEvents() {
-			var appodealManager = GetType("YOUR_EVENT_MANAGER_CLASS_HERE"); // TODO Replace with your namespace + class name
-			if (appodealManager != null) {
-				this.bindEvents(appodealManager, typeof(AppodealEvents));
+			var appodealInterstitialManager = GetType("com.appodeal.ads.InterstitialCallbacks");
+			if (appodealInterstitialManager != null) {
+				this.bindEvents(appodealInterstitialManager, typeof(AppodealInterstitialEvents));
+			}
+			var appodealVideoManager = GetType("com.appodeal.ads.SkippableVideoCallbacks");
+			if (appodealVideoManager != null) {
+				this.bindEvents(appodealVideoManager, typeof(AppodealVideoEvents));
+			}
+			var appodealRewardedVideoManager = GetType("com.appodeal.ads.RewardedVideoCallbacks");
+			if (appodealRewardedVideoManager != null) {
+				this.bindEvents(appodealRewardedVideoManager, typeof(AppodealRewardedVideoEvents));
+			}
+			var appodealBannerManager = GetType("com.appodeal.ads.BannerCallbacks");
+			if (appodealBannerManager != null) {
+				this.bindEvents(appodealBannerManager, typeof(AppodealBannerEvents));
 			}
 		}
 
-		private class AppodealEvents {
-
-			// Here you need to define the events you have.
-			// Make sure that the name and parameter types match the event.
-
-			public static void EVENT_EXAMPLE() {
+		private class AppodealInterstitialEvents {
+			public void onInterstitialLoaded(bool isPrecache) {
 				AppodealGrowIntegration.Instance.OnAdLoaded(AdType.INTERSTITIAL);
 			}
+			public void onInterstitialFailedToLoad() {
+				AppodealGrowIntegration.Instance.OnAdFailedToLoad(AdType.INTERSTITIAL);
+			}
+			public void onInterstitialShown() {
+				AppodealGrowIntegration.Instance.OnAdDisplayed(AdType.INTERSTITIAL);
+			}
+			public void onInterstitialClicked() {
+				AppodealGrowIntegration.Instance.OnAdClicked(AdType.INTERSTITIAL);
+			}
+			public void onInterstitialClosed() {
+				AppodealGrowIntegration.Instance.OnAdLoaded(AdType.INTERSTITIAL);
+			}
+		}
 
+		private class AppodealVideoEvents {
+			public void onSkippableVideoLoaded() {
+				AppodealGrowIntegration.Instance.OnAdLoaded(AdType.VIDEO);
+			}
+			public void onSkippableVideoFailedToLoad() {
+				AppodealGrowIntegration.Instance.OnAdFailedToLoad(AdType.VIDEO);
+			}
+			public void onSkippableVideoShown() {
+				AppodealGrowIntegration.Instance.OnAdDisplayed(AdType.VIDEO);
+			}
+			public void onSkippableVideoFinished() {
+				AppodealGrowIntegration.Instance.OnRewardedVideoCompleted(AdType.VIDEO);
+			}
+			public void onSkippableVideoClosed() {
+				AppodealGrowIntegration.Instance.OnAdClosed(AdType.VIDEO);
+			}
+		}
+
+		private class AppodealRewardedVideoEvents {
+			public void onRewardedVideoLoaded() {
+				AppodealGrowIntegration.Instance.OnAdLoaded(AdType.REWARDED_VIDEO);
+			}
+			public void onRewardedVideoFailedToLoad() {
+				AppodealGrowIntegration.Instance.OnAdFailedToLoad(AdType.REWARDED_VIDEO);
+			}
+			public void onRewardedVideoShown() {
+				AppodealGrowIntegration.Instance.OnAdDisplayed(AdType.REWARDED_VIDEO);
+			}
+			public void onRewardedVideoFinished(int amount, string name) {
+				AppodealGrowIntegration.Instance.OnRewardedVideoCompleted(AdType.REWARDED_VIDEO, name, (float)amount);
+			}
+			public void onRewardedVideoClosed() {
+				AppodealGrowIntegration.Instance.OnAdClosed(AdType.REWARDED_VIDEO);
+			}
+		}
+
+		private class AppodealBannerEvents {
+			public void onBannerLoaded(bool isPrecache) {
+				AppodealGrowIntegration.Instance.OnAdLoaded(AdType.BANNER);
+			}
+			public void onBannerFailedToLoad() {
+				AppodealGrowIntegration.Instance.OnAdFailedToLoad(AdType.BANNER);
+			}
+			public void onBannerShown() {
+				AppodealGrowIntegration.Instance.OnAdDisplayed(AdType.BANNER);
+			}
+			public void onBannerClicked() {
+				AppodealGrowIntegration.Instance.OnAdClicked(AdType.BANNER);
+			}
 		}
 	}
 }
